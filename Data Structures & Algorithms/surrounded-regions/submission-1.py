77a434796ebb4279497b86612839b241132007e0
ZheminHuang <160550427+ZheminHuang@users.noreply.github.com>
2026-08-19 09:50:37 -0600

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows = len(board)
+        cols = len(board[0])
+
+        queue = deque()
+
+
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+
+        for r in range(rows):
+            if board[r][0]=="O":
+                board[r][0] = "T"
+                queue.append((r, 0))
+
+            if board[r][cols - 1] == "O":
+                board[r][cols - 1] = "T"
+                queue.append((r, cols - 1))
+        
+        for c in range(cols):
+            if board[0][c] == "O":
+                board[0][c] = "T"
+                queue.append((0, c))
+
+            if board[rows - 1][c] == "O":
+                board[rows - 1][c] = "T"
+                queue.append((rows - 1, c))
+
+
+        while queue:
+            r,c = queue.popleft()
+
+            for dr,dc in directions:
+                nr = r+dr
+                nc = c+dc
+
+                if (0<=nr<rows and 0<=nc<cols and board[nr][nc]=="O"):
+                    board[nr][nc]="T"
+                    queue.append((nr,nc))
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="O":
+                    board[r][c]="X"
+
+                elif board[r][c] == "T":
+                    board[r][c] = "O"                
