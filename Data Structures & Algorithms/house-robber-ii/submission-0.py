54ff7face45dab8c8144b4016ca16bc7e6d3433b
ZheminHuang <160550427+ZheminHuang@users.noreply.github.com>
2026-08-22 11:53:55 -0600

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+
+        def rob_line(houses):
+            if len(houses)==1:
+                return houses[0]
+            
+            dp = [0]*len(houses)
+
+            dp[0] = houses[0]
+            dp[1] = max(houses[0],houses[1])
+
+            for i in range(2,len(houses)):
+                dp[i] = max(dp[i-1],dp[i-2]+houses[i])
+            
+            return dp[-1]
+
+        return max(rob_line(nums[:-1]),rob_line(nums[1:]))
