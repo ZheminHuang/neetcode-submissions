45f9f8f452e2d07cc6d9b0495e92af4e55ad76e0
ZheminHuang <160550427+ZheminHuang@users.noreply.github.com>
2026-09-06 21:32:38 -0400

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+
+        for src,dst in tickets:
+            heapq.heappush(graph[src],dst)
+        
+
+        result = []
+
+        def dfs(airport):
+            while graph[airport]:
+                next_airport = heapq.heappop(graph[airport])
+                dfs(next_airport)
+            
+            result.append(airport)
+        
+        dfs("JFK")
+    
+        return result[::-1]
